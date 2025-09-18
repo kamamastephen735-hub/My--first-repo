9bfefc623511f8934178555639d66ba27117b41e
kamamastephen735-hub <kamamastephen735@gmail.com>
2025-09-18 13:21:18 +0300

    Add files via upload

---
+++ C program for semester.c
@@ -0,0 +1,21 @@
+// C program for semester units
+/*Name: Stephen musembi kamama
+Registration number: CT100/G/26235/25
+Age :20 years 
+phone number: 0757743735
+Course: BSc IT
+Unit:SPC 2101 
+Description:Units for this semester 
+*/
+ #include <stdio.h>
+int main(){
+printf("SPI2102 computer system and organisation\n");
+printf ("UCU2105 Fundamentals of ICT \n");
+printf ("SPI 2120 Essential of accounting \n");
+printf ("SPH 2107 Analogue electronics \n");
+printf("SPI2103 introduction to computer application \n");
+printf ("UCU2101 Communication skills \n");
+printf ("SPM2104 mathematics For science \n");
+printf("SPI2101 introduction to programming and algorithms \n");
+return 0;
+}
---
+++ c program assignment.c
@@ -0,0 +1,27 @@
+//Simple C program my name
+/*
+Name: Stephen M Kamama
+Age: 20 years 
+Sex: Male
+Course: BSc IT
+Registration number: CT100/G/26235/25
+Description: Assignment given
+*/
+#include <stdio.h>
+
+int main(){
+int height;
+printf("Enter your height;\n*");
+scanf("%d", &height);
+printf("Your height is %d \n*, height");
+
+int weight ;
+printf("*\n Enter your weight; \n*");
+scanf("%d", &weight);
+printf("Your weight is %d \n*, weight");
+
+int phonenumber;
+printf("\n Enter your phonenumber:\n");
+scanf("%d", &phonenumber);
+printf("Your phonenumber is %d", &phonenumber);
+return 0;}
