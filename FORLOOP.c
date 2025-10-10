15725a0f92249833d6c73202c762b62e42e977b5
kamamastephen735-hub <kamamastephen735@gmail.com>
2025-10-10 06:58:13 +0300

    Add files via upload

---
+++ FORLOOP.c
@@ -0,0 +1,17 @@
+/*
+Name STEPHEN M KAMAMA
+Registration number CT100/G/26235/25
+Course BSC IT
+Description A C PROGRAM, FOR LOOP
+*/
+
+#include <stdio.h>
+
+int main() {
+    // Print numbers from 100 down to 50
+    for(int i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
